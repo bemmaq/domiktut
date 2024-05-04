4279303abf6b0add95c6cfc2348b18809f9200e0
Begimai <bahadurybabegimai@gmail.com>
2024-05-04 13:44:36 +0600

    hahaton

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Catalog from './pages/Catalog/Catalog'
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/catalog' element={<Catalog/>}/>
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Helpers/Container.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Container = () => {
+  return (
+    <div className='1200px m-auto'></div>
+  )
+}
+
+export default Container
