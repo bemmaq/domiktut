37ebdb1417fbfc93714e3d242a919ad8ab16ba76
Sherniaz7777 <sherniazibragimov10@gmail.com>
2024-05-06 15:15:08 +0600

    love

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Catalog from './pages/Catalog/Catalog'
+import Love from './pages/Love/Love'
 
 const App = () => {
   return (
     <div>
-      <Catalog/>
+      <Love/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
